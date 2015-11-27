82ac19d68e1c19e81b53e260e3cfd40f6c212419
Null <Null>
2015-11-27 09:42:16 +0000

    Uploading Blog
    Trying to Overrite all my files, might mess up again?

---
+++ web/results.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+	<title>Results</title>
+	<link rel="stylesheet" type="text/css" href="css/poll.css"/>
+</head>
+
+<body>
+	<table id="tblResultsMain" align="center">
+	     <tr>
+		<td class="header">The Results to the electorial Results</td>
+	     </tr>
+	     <tr>
+		<td>
+		     <?php
+			include "savevote.php";
+		     ?>
+		</td>
+	     </tr>
+	     <tr>
+			<td class="footer">We are Anonymous.
+								We are Legion.
+								We do not forgive.
+								We do not forget.
+								Expect us.
+</a></td>
+	     </tr>
+	</table>
+</body>
+
+</html>
